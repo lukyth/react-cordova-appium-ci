6d48b5c056a605b6b247155540d1f2208577098a
Kanitkorn S <k.sujautra@gmail.com>
2017-05-15 16:52:31 +0700

    Refactor test for multi-platform

--- cordova/e2e/src/test.js
+++ cordova/e2e/src/test.js
@@ -6,70 +6,50 @@ const it = require('mocha').it
 require('./helpers/setup')
 
 const wd = require('wd')
-
-describe('android', function () {
-  this.timeout(300000)
-  const serverConfig = {
-    host: 'localhost',
-    port: 4723
+const platforms = [
+  {
+    browserName: '',
+    deviceName: 'any value; Appium uses the first device from *adb devices*',
+    platformName: 'Android',
+    app: '/Users/kanitkorn/Code/react-cordova-appium-ci/cordova/platforms/android/build/outputs/apk/android-x86-debug.apk',
+    androidDeviceSocket: 'com.kanitkorn.reactcordova_devtools_remote',
+    chromeOptions: {
+      androidDeviceSocket: 'com.kanitkorn.reactcordova_devtools_remote'
+    }
+  },
+  {
+    automationName: 'XCUITest',
+    browserName: '',
+    deviceName: 'iPhone 5s',
+    platformName: 'iOS',
+    app: '/Users/kanitkorn/Code/react-cordova-appium-ci/cordova/platforms/ios/build/emulator/ReactCordova.app'
   }
-  const driver = wd.promiseChainRemote(serverConfig)
-  require('./helpers/logging').configure(driver)
+]
 
-  before(() => driver
-    .init({
-      browserName: '',
-      deviceName: 'any value; Appium uses the first device from *adb devices*',
-      platformName: 'Android',
-      app: '/Users/kanitkorn/Code/react-cordova-appium-ci/cordova/platforms/android/build/outputs/apk/android-x86-debug.apk',
-      androidDeviceSocket: 'com.kanitkorn.reactcordova_devtools_remote',
-      chromeOptions: {
-        androidDeviceSocket: 'com.kanitkorn.reactcordova_devtools_remote'
-      }
-    })
-    .setImplicitWaitTimeout(5000)
-    .sleep(5000)
-    .contexts()
-    .then(context => driver.context(context[1]))
-  )
+platforms.forEach(platform => {
+  describe(platform.platformName, function () {
+    this.timeout(300000)
+    const serverConfig = {
+      host: 'localhost',
+      port: 4723
+    }
+    const driver = wd.promiseChainRemote(serverConfig)
+    require('./helpers/logging').configure(driver)
 
-  after(() => driver.quit())
+    before(() => driver
+      .init(platform)
+      .setImplicitWaitTimeout(5000)
+      .sleep(5000)
+      .contexts()
+      .then(context => driver.context(context[1]))
+    )
 
-  it('should find header elements', () => driver
-    .elementByCss('.App-logo')
-    .elementByCss('.App-header')
-      .text().should.become('Welcome to React')
-  )
-})
+    after(() => driver.quit())
 
-describe('ios', function () {
-  this.timeout(300000)
-  const serverConfig = {
-    host: 'localhost',
-    port: 4723
-  }
-  const driver = wd.promiseChainRemote(serverConfig)
-  require('./helpers/logging').configure(driver)
-
-  before(() => driver
-    .init({
-      automationName: 'XCUITest',
-      browserName: '',
-      deviceName: 'iPhone 5s',
-      platformName: 'iOS',
-      app: '/Users/kanitkorn/Code/react-cordova-appium-ci/cordova/platforms/ios/build/emulator/ReactCordova.app'
-    })
-    .setImplicitWaitTimeout(5000)
-    .sleep(5000)
-    .contexts()
-    .then(context => driver.context(context[1]))
-  )
-
-  after(() => driver.quit())
-
-  it('should find header elements', () => driver
-    .elementByCss('.App-logo')
-    .elementByCss('.App-header')
-      .text().should.become('Welcome to React')
-  )
+    it('should find header elements', () => driver
+      .elementByCss('.App-logo')
+      .elementByCss('.App-header')
+        .text().should.become('Welcome to React')
+    )
+  })
 })
